0762a557c0e3939a6bac5e67474013c226d55ec2
Akhil <kashyapakhil826@gmail.com>
2022-05-14 22:48:28 +0530

    Cart-additional

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import Express from 'express'
+import express from 'express'
 import data from './data.js'
 
-const app = Express()
+const app = express()
 
 app.get('/api/products', (req, res) => {
   res.send(data.products)
@@ -9,6 +9,15 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug)
+  if (product) {
+    res.send(product)
+  } else {
+    res.status(404).send({ message: 'Product Not Found' })
+  }
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id)
   if (product) {
     res.send(product)
   } else {
