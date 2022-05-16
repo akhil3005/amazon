1a01c77a157a3501bd99bb3e8014f288fca1db00
Akhil <kashyapakhil826@gmail.com>
2022-05-16 23:11:25 +0530

    Pay-order-by-PayPal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
@@ -12,10 +13,13 @@ root.render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
-        <App />
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
       </HelmetProvider>
     </StoreProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
