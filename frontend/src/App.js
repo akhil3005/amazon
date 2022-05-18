69da4024bd6bccde1fc36eb5fed001a9f273544f
Akhil <kashyapakhil826@gmail.com>
2022-05-18 09:06:23 +0530

    List-Orders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ import AdminRoute from './components/AdminRoute'
 import DashboardScreen from './screens/DashboardScreen'
 import ProductListScreen from './screens/ProductListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
+import OrderListScreen from './screens/OrderListScreen'
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store)
@@ -202,6 +203,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/orders"
+                element={
+                  <AdminRoute>
+                    <OrderListScreen />
                   </AdminRoute>
                 }
               ></Route>
